d7d341b217d753b1b6788874b1765622ee7cc7f3
Ubuntu <ubuntu@ip-172-31-91-123.ec2.internal>
2020-08-05 16:41:01 +0000

    Added in basic_math.py and area_and_perimeter.py

---
+++ area_and_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by Marcus A. Mosley
+# Created on August 2020
+# This program calculates the area and perimeter of a rectangle
+#   with dimensions 5cm x 3cm
+
+
+def main():
+    # This function calculates the area and perimeter
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(3*5))
+    print("Perimeter is {}cm".format(2*(3+5)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by Marcus A. Mosley
+# Created on August 2020
+# This program does basic math
+
+
+def main():
+    # This function does basic math
+    print(9+2)
+    print("7-3={}".format(7-3))
+    print("4/2={}".format(4/2))
+    print("4+4/2={}".format(4+4/2))
+    print("3+4*2={}".format(3+4*2))
+    print("5+2^3={}".format(5+2^3)) # Bitwise Operators
+    print("5+2**3={}".format(5+2**3)) # Exponents
+
+
+if __name__ == "__main__":
+    main()
