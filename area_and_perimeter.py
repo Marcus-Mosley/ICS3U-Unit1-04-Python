af0a4b0aaaa1a9488a2179c5864b0ceac8ee1a71
Marcus-Mosley <68971666+Marcus-Mosley@users.noreply.github.com>
2020-08-10 17:03:54 -0400

    Update area_and_perimeter.py

--- area_and_perimeter.py
+++ area_and_perimeter.py
@@ -11,8 +11,8 @@ def main():
     print("If a rectangle has the dimensions:")
     print("5cm x 3cm")
     print("")
-    print("Area is {}cm^2".format(3*5))
-    print("Perimeter is {}cm".format(2*(3+5)))
+    print("Area is {}cm^2".format(5*3))
+    print("Perimeter is {}cm".format(2*(5+3)))
 
 
 if __name__ == "__main__":
